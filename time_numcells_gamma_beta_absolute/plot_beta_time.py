dad69a30b285df8b831c8f7d59fc65f16551cece
Randy Heiland <randy.heiland@gmail.com>
2026-06-17 19:58:41 -0400

    update

--- time_numcells_gamma_beta_absolute/plot_beta_time.py
+++ time_numcells_gamma_beta_absolute/plot_beta_time.py
@@ -20,24 +20,28 @@ ax0 = fig.gca()
 # print("type(beta_vals)=", type(beta_vals))
 # print(beta_vals)
 
-beta_vals = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+# beta_vals = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
 tvals = []
 bvals = []
+eps = 0.0001
 for gamma in [0.0]:
     # idx = -1
     # for gamma in np.arange(0.0, 1.05, 0.01):   # 100 samples
     # for gamma in np.arange(0.0, 0.10, 0.043):   # 100 samples
     # for beta in beta_vals[:-1]:
-    for beta in beta_vals:
-        folder_name = "out_num_cells_b" + str(beta) + "_g" + str(gamma)
+    # for beta in beta_vals:
+    # if primary_pass:
+    for beta in np.arange(0.1, 0.95+eps, 0.01):
+        b2 = int((beta+eps) * 100) / 100
+        folder_name = "out_num_cells_b" + str(b2) + "_g" + str(gamma)
         if (not os.path.exists(folder_name)):
             print("--- ERROR missing dir: ", folder_name)
             sys.exit(-1)
 
-        label = "b:"+str(beta) + ",g:"+str(gamma)
+        label = "b:"+str(b2) + ",g:"+str(gamma)
         # idx += 1
 
-        data_dir = "out_num_cells_b" + str(beta) + "_g" + str(gamma)
+        data_dir = "out_num_cells_b" + str(b2) + "_g" + str(gamma)
         print('data_dir = ',data_dir)
 
         os.chdir(data_dir)
@@ -61,7 +65,47 @@ for gamma in [0.0]:
 
         if final_time > 0:
             tvals.append(final_time)
-            bvals.append(beta)
+            bvals.append(b2)
+        else:
+            print(f"  --- bogus time {final_time} in {data_dir} ")
+
+
+    # extra runs
+    for beta in [0.996, 0.997, 0.998, 0.999]:
+        b2 = beta
+        folder_name = "out_num_cells_b" + str(b2) + "_g" + str(gamma)
+        if (not os.path.exists(folder_name)):
+            print("--- ERROR missing dir: ", folder_name)
+            sys.exit(-1)
+
+        label = "b:"+str(b2) + ",g:"+str(gamma)
+        # idx += 1
+
+        data_dir = "out_num_cells_b" + str(b2) + "_g" + str(gamma)
+        print('data_dir = ',data_dir)
+
+        os.chdir(data_dir)
+        xml_files = glob.glob('output*.xml')
+        os.chdir('..')
+        xml_files.sort()
+        # print('xml_files = ',xml_files)
+
+        ds_count = len(xml_files)
+        # print("ds_count = ",ds_count)
+        # ds_count = 192
+        # print("----- ds_count = ",ds_count)
+        mcds = [pyMCDS_cells(xml_files[i], data_dir) for i in range(ds_count)]
+
+        tval = np.linspace(0, mcds[-1].get_time(), ds_count)
+
+        tval /= cell_cycle_duration
+        # print("tval= ",tval)
+        final_time = tval[-1]
+        print(f'{data_dir} final time= {final_time}')
+
+        if final_time > 0:
+            tvals.append(final_time)
+            bvals.append(b2)
         else:
             print(f"  --- bogus time {final_time} in {data_dir} ")
 
@@ -81,7 +125,7 @@ ax0.plot(bvals,tvals,'.-', color='k')
 
 ax0.set_title("gamma=0", fontsize=12)
 ax0.set_xlabel('beta')
-ax0.set_xlim(0., 1.)
+ax0.set_xlim(0., 1.1)
 ax0.set_ylabel('Time (calibrated for 5T)')
 # ax0.savefig(data_dir + '.png')
 plt.show()

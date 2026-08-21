e47b9f51e3c682ccaf424ba89e29bda116081680
Randy Heiland <randy.heiland@gmail.com>
2026-08-21 19:09:18 -0400

    add g0.0 time series

---
+++ time_numcells_gamma_beta_absolute/zip_b0_series.sh
@@ -0,0 +1,10 @@
+
+#(base) M1P~/git/PhysiCell_monolayer/time_numcells_gamma_beta_absolute$ 
+
+zip -r pc_series_b0.0_g0.0026.zip series_b0.0_g0.0026
+zip -r pc_series_b0.0_g0.317.zip series_b0.0_g0.317
+zip -r pc_series_b0.0_g0.7266.zip series_b0.0_g0.7266
+zip -r pc_series_b0.0_g0.887.zip series_b0.0_g0.887
+zip -r pc_series_b0.0_g0.933.zip series_b0.0_g0.933
+
+
---
+++ time_numcells_gamma_beta_absolute/zip_g0_series.sh
@@ -0,0 +1,8 @@
+
+#(base) M1P~/git/PhysiCell_monolayer/time_numcells_gamma_beta_absolute$ 
+
+zip -r pc_series_b0.663_g0.0.zip  series_b0.663_g0.0
+zip -r pc_series_b0.925_g0.0.zip  series_b0.925_g0.0
+zip -r pc_series_b0.9889_g0.0.zip  series_b0.9889_g0.0
+zip -r pc_series_b0.9976_g0.0.zip  series_b0.9976_g0.0
+zip -r pc_series_b0.9992_g0.0.zip  series_b0.9992_g0.0
